f5278f6989a23d5b437cba3dd2260cfe7051e635
Renan Kosicki <renankosicki@Mac-mini-de-Renan.local>
2015-09-29 14:34:09 -0300

    Logic added of filling path

--- exploding-monkeys/exploding-monkeys/GameScene.swift
+++ exploding-monkeys/exploding-monkeys/GameScene.swift
@@ -8,9 +8,19 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
+enum CollisionTypes: UInt32 {
+    case Banana = 1
+    case Building = 2
+    case Player = 4
+}
 
+class GameScene: SKScene {
+    var buildings = [BuildingNode]()
+
+    override func didMoveToView(view: SKView) {
+        backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
+        
+        createBuildings()
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -19,5 +29,21 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    func createBuildings() {
+        var currentX: CGFloat = -15
+        
+        while currentX < 1024 {
+            let size = CGSize(width: RandomInt(min: 2, max: 4) * 40, height: RandomInt(min: 300, max: 600))
+            currentX += size.width + 2
+            
+            let building = BuildingNode(color: UIColor.redColor(), size: size)
+            building.position = CGPoint(x: currentX - (size.width / 2), y: size.height / 2)
+            building.setup()
+            addChild(building)
+            
+            buildings.append(building)
+        }
     }
 }
